2e2cef8866284fef2163463a2e0e27a54270d8c1
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-11-02 16:58:16 +0300

    added response survey card

---
+++ src/components/responses/ResponseSurveyCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ResponseSurveyCard() {
+  return (
+    <div>
+        <h3>Buraya doldurulmuş anketler gelecek</h3>
+    </div>
+  )
+}
+
+export default ResponseSurveyCard
