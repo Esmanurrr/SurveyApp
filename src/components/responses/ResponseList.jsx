2bdf44301972ed30b38156740eaf8670f01b8609
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-12-04 11:00:02 +0300

    added response list

--- src/components/responses/ResponseList.jsx
+++ src/components/responses/ResponseList.jsx
@@ -1,16 +1,48 @@
-import React from 'react'
-import ResponseCard from './ResponseCard'
-import { useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "../../firebase";
+import ResponseSurveyCard from "./ResponseSurveyCard";
+import LoadingPage from "../infos/LoadingPage";
 
 function ResponseList() {
-    const location = useLocation();
-    
-    console.log(location.state.responses);
+  const location = useLocation();
+  const [responsesList, setResponsesList] = useState([]);
+
+  useEffect(() => {
+    const fetchResponses = async () => {
+      try {
+        const q = query(collection(db, "responses"));
+        const querySnapshot = await getDocs(q);
+        const fetchedResponses = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setResponsesList(fetchedResponses);
+      } catch (error) {
+        console.log("yanıtlar alınamadı: ", error);
+      }
+    };
+
+    fetchResponses();
+  }, []);
+
   return (
-    <div>
-        <ResponseCard />
-    </div>
-  )
+    <>
+      {responsesList.length > 0 ? (
+        responsesList.map((response) => (
+          <ResponseSurveyCard
+            key={response.id}
+            id={response.id}
+            title={response.title}
+          />
+        ))
+      ) : (
+        <LoadingPage />
+      )}
+    </>
+  );
 }
 
-export default ResponseList
+export default ResponseList;
--- src/components/responses/Responses.jsx
+++ src/components/responses/Responses.jsx
@@ -1,85 +1,59 @@
-import { useEffect, useState } from 'react'
-import { BaseBackground, Container, FlexContainer, RelativeDiv } from '../../style'
-import CreateSurvey from '../dashboard/CreateSurvey'
-import CreateSurveyModal from '../dashboard/CreateSurveyModal'
-import { createPortal } from 'react-dom'
-import ResponseSurveyCard from './ResponseSurveyCard'
-import { collection, doc, getDocs, query } from 'firebase/firestore'
-import { db } from '../../firebase'
+import { useState } from "react";
+import {
+  BaseBackground,
+  Container,
+  FlexContainer,
+  RelativeDiv,
+} from "../../style";
+import CreateSurvey from "../dashboard/CreateSurvey";
+import CreateSurveyModal from "../dashboard/CreateSurveyModal";
+import { createPortal } from "react-dom";
+import ResponseList from "./ResponseList";
 // import { useNavigate } from 'react-router-dom'
 
 function Responses() {
-    const [portal, setPortal] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const [responsesList, setResponsesList] = useState([]);
+  const [portal, setPortal] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-      const fetchResponses = async () => {
-        try {
-          const q = query(collection(db, "responses"));
-          const querySnapshot = await getDocs(q);
-          const fetchedResponses = querySnapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data()
-          }));
-
-          setResponsesList(fetchedResponses);
-        } catch (error) {
-          console.log("yanıtlar alınamadı: ", error);
-        }
-      }
-
-      fetchResponses();
-    }, []);
-
-    const handlePortal = async () => {
-      setLoading(true);
-      try {
-        setPortal(true);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
+  const handlePortal = async () => {
+    setLoading(true);
+    try {
+      setPortal(true);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
-  
-    const closePortal = () => {
-      setPortal(false);
-    }
-  
- 
+  };
+
+  const closePortal = () => {
+    setPortal(false);
+  };
+
   return (
     <BaseBackground>
-        <Container>
-          <RelativeDiv>
-              {
-                !loading && 
-                <FlexContainer>
-                  <div style={{flexGrow: 1}}>
-                    {
-                      responsesList.length > 0 ? (
-                        responsesList.map((response) => (
-                          <ResponseSurveyCard 
-                            key={response.id}
-                            id={response.id}
-                            title={response.title}
-                          />
-                        ))
-                      ) : (
-                        <p>Henüz doldurulmuş bir anket bulunmamaktadır</p>
-                      )
-                    }
-                  </div>
-                  <div style={{flexShrink: 0, marginTop: "1rem"}}>
-                  <CreateSurvey handlePortal={handlePortal} />
-                  {portal && createPortal(<CreateSurveyModal closePortal={closePortal}/>, document.body)}
-                  </div>
-                </FlexContainer>
-              }
-          </RelativeDiv>
-        </Container>
+      <Container>
+        <h1>Responses</h1>
+        <RelativeDiv>
+          {!loading && (
+            <FlexContainer>
+              <div style={{ flexGrow: 1 }}>
+                <ResponseList />
+              </div>
+              <div style={{ flexShrink: 0, marginTop: "1rem" }}>
+                <CreateSurvey handlePortal={handlePortal} />
+                {portal &&
+                  createPortal(
+                    <CreateSurveyModal closePortal={closePortal} />,
+                    document.body
+                  )}
+              </div>
+            </FlexContainer>
+          )}
+        </RelativeDiv>
+      </Container>
     </BaseBackground>
-  )
+  );
 }
 
-export default Responses
+export default Responses;
