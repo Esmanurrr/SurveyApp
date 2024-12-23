904a9ba83414df85c108f48986431a1c43f5cc37
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-12-23 14:54:01 +0300

    updated minor ui changes

--- src/components/infos/SurveyComplete.jsx
+++ src/components/infos/SurveyComplete.jsx
@@ -1,7 +1,14 @@
+import { Container, TextCenter } from "../../style"
 
 function SurveyComplete() {
   return (
-    <div>Survey is completed</div>
+    <Container>
+    <TextCenter>
+      <i className="fa-regular fa-circle-check fa-2xl" style={{color: "#0071e2", margin: "1rem"}}></i>
+      <h3>Survey is completed. Thank you!</h3>
+      <p>You can close the window.</p>
+    </TextCenter>
+    </Container>
   )
 }
 
